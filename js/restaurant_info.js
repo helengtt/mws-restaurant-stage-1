bbe560761bdb58551c20e6cff3088d5eeb63b33f
Helen Guo <helengreen2008@gmail.com>
2018-12-10 16:12:26 +1100

    feat: responsive design under mobile phone

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiaGVsZW5ndHQiLCJhIjoiY2pwY2k2amNvMDI3cTNwbzgxZTFza2dtNyJ9.F4E8b7vOlNMnlTozd7-pcA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -150,14 +150,18 @@ createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
   name.innerHTML = review.name;
+  li.setAttribute("class", "reviews-item");  
+  name.setAttribute("class", "reviews-name");  
   li.appendChild(name);
 
   const date = document.createElement('p');
   date.innerHTML = review.date;
+  date.setAttribute("class", "reviews-date");
   li.appendChild(date);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
+  rating.setAttribute("class", "reviews-rating");  
   li.appendChild(rating);
 
   const comments = document.createElement('p');
