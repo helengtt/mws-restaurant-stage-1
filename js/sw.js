0eaced89c3c0655fb2a630ff430071341968527c
Helen Guo <helengreen2008@gmail.com>
2018-12-12 12:41:00 +1100

    feat: update service worker

--- js/sw.js
+++ js/sw.js
@@ -1,5 +1,6 @@
-var CACHE_NAME = 'restaurant-review';
+var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
+    '/',
     '/index.html',
     '/restaurant.html',
     '/css/styles.css',
@@ -23,20 +24,31 @@ var urlsToCache = [
 
 self.addEventListener('install', function(event){
     event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function(cache){
+        caches.open(CACHE_NAME).then(function(cache){
+            console.log('Opened cache');
             return cache.addAll(urlsToCache);   
         })
     );
 });
 
 self.addEventListener('activate', function(event){
-
+    var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+    event.waitUntil (
+        caches.keys().then(function(cacheNames){
+            return Promise.all(
+                cacheNames.map(function(cacheName){
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })                
+            )
+        })
+    );
 });
 
 self.addEventListener('fetch', function() {
     event.respondWith(
-        catches.match(event.request).then(function(response){
+        caches.match(event.request).then(function(response){
             if (response) return response;
             return fetch(event.request);
         })
